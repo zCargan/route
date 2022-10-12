ee106138067b3bb9eb14e06722355592f316a843
Logan <lo.carlier@students.ephec.be>
2022-10-12 23:14:07 +0200

    test /ajout de hello

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../images//logoNewMe2.ico';
 import "../styles/navbar.css"
@@ -8,52 +8,55 @@ function Navbar() {
 
     const handleClick = () => setClick(!click);
 
-  return (
-    <nav className={click ? 'navbar-active' : 'navbar'}>
-        <div className='navbar-container'>
-            <div className="menu-list">
-                <div className="menu-icon" onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+    return (
+        <nav className={click ? 'navbar-active' : 'navbar'}>
+            <div className='navbar-container'>
+                <div className="menu-list">
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <NavLink to="/" className="navbar-logo">
+                        <img src={logo} alt="NewMeLogo" />
+                    </NavLink>
                 </div>
-                <NavLink to="/" className="navbar-logo">
-                    <img src={logo} alt="NewMeLogo"/>
-                </NavLink>
+                <p>
+                    Hello
+                </p>
+                <ul className="nav-list">
+                    <li className='nav-item'>
+                        <NavLink to='/home' className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink to="/objectifs" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
+                            Objectifs
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink to="/carte" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
+                            Carte
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink to="/adefinir" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
+                            ?
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink to="/api" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
+                            Profil
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink to="/inscription" className={(state) => state.isActive ? 'nav-links-mobile-active' : 'nav-links-mobile'}>
+                            Sign Up
+                        </NavLink>
+                    </li>
+                </ul>
             </div>
-            <ul className="nav-list">
-                <li className='nav-item'>
-                    <NavLink to='/home' className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
-                        Home
-                    </NavLink>
-                </li>
-                <li className='nav-item'>
-                    <NavLink to="/objectifs" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
-                        Objectifs
-                    </NavLink>
-                </li>
-                <li className='nav-item'>
-                    <NavLink to="/carte" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
-                        Carte
-                    </NavLink> 
-                </li>
-                <li className='nav-item'>
-                    <NavLink to="/adefinir" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
-                        ?
-                    </NavLink>
-                </li>
-                <li className='nav-item'>
-                    <NavLink to="/api" className={(state) => state.isActive ? 'nav-links-active' : 'nav-links'}>
-                        Profil
-                    </NavLink>
-                </li>
-                <li className='nav-item'>
-                    <NavLink to="/inscription" className={(state) => state.isActive ? 'nav-links-mobile-active' : 'nav-links-mobile'}>
-                        Sign Up
-                    </NavLink>
-                </li>
-            </ul> 
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
