b0aeb1fcbad841254912a08712c168f228c6087e
zCargan <64102236+zCargan@users.noreply.github.com>
2022-10-16 18:24:18 +0200

    Add files via upload

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const User = require("../models/user");
 
 const app = require("../app");
@@ -193,4 +194,22 @@ exports.getUserCity = (req, res) => {
       res.status(200).json(response)
       console.log(response)
   })
+=======
+const User = require("../models/user");
+const app = require("../app");
+
+exports.updateObjectif = (req, res, next) => {
+    let keys = Object.keys(req.body)
+    let values = Object.values(req.body)
+    let prenom_key = keys[0];
+    let prenom_value = values[0];
+    let prenom_json = {[prenom_key]:prenom_value}
+    let objectifs_key = keys[1];
+    let objectifs_value = values[1];
+    let objectifs_json = {[objectifs_key]:objectifs_value};
+    console.log(prenom_json, objectifs_json)
+    User.updateOne(prenom_json, {$set:objectifs_json})
+    .then(() => res.status(201).json({ message: 'Utilisateur modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+>>>>>>> 2fad845 (Add files via upload)
 }
