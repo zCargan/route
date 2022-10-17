de1fe6a426a52a5240f4b964a37a6f155a993035
Logan <lo.carlier@students.ephec.be>
2022-10-17 20:48:15 +0200

    ajout de la carte

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 
@@ -12,3 +13,21 @@ const userSchema = mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
+=======
+const mongoose = require('mongoose');
+//const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    confirmed: { type: Boolean, required: true },
+    objectif: { type: Array, required: true },
+    city: { type: String, require: false }
+});
+
+//userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
+
+>>>>>>> 4b50560 (ajout de la carte)
