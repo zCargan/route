214e03d769485a90dc447baafe389bbe47c9caf3
Logan <lo.carlier@students.ephec.be>
2022-12-14 23:52:01 +0100

    Ajout d'une vidéo de présentation
    Vidéo de présentation sur la page default.js servant à décrire le site
    NewMe.

--- frontend/src/pages/default.js
+++ frontend/src/pages/default.js
@@ -1,28 +1,56 @@
 import React from 'react'
 import "../styles/default.css"
 import RGPD from '../components/RGPD';
+import ReactPlayer from 'react-player'
 
 
 const Default = () => {
 
     return (
-        <div className='div_global'>
-            <div>
-                <p className='bienvenue_text'>
-                    NEWME
-                </p>
-            </div>
-            <div>
-                <p>
-                    NewMe est un projet étudiant dans le cadre d'un cours.
+        <div>
+            <div className='div_global'>
+                <div>
+                    <p className='bienvenue_text'>
+                        NEWME
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        NewMe est un projet étudiant dans le cadre d'un cours.
 
-                    Nous avons voulu pousser la communauté 
-                </p>
-            </div>
-            <div>
-                <RGPD />
-            </div>
-        </div >
+                        Nous avons voulu pousser la communauté
+                        <div>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                        </div>
+                    </p>
+                </div>
+                <div className='div_video'>
+                    <ReactPlayer className="video"
+                        url="https://www.youtube.com/watch?v=VFNMX8s2Q10"
+                        playing={true}
+                        onStart={true}
+                        onReady={true}
+                        width="100%"
+                        height="562px"
+                        volume={0.03}
+                    />
+                </div>
+                <div>
+                    <RGPD />
+                </div>
+            </div >
+        </div>
     )
 }
 
